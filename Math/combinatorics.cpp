695d11798270dda028b30b0e6856595d06ea08cd
The_Hallak <sedamaa777mmm@gmail.com>
2023-09-07 17:44:22 +0300

    add combinatorics

---
+++ Math/combinatorics.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+//recursion; O(log(n))
+ll pow(ll x,ll n,ll mod){
+    if(!n)return 1;
+    if(n==1)return x%mod;
+    ll res=pow(x,n/2,mod);
+    res=res*res%mod;
+    if(n%2)res=res*x%mod;
+    return res;
+}
+
+//iterative; O(log(n))
+ll pow(ll x,ll n,ll mod){
+    x%=MOD;
+    ll res=1;
+    while(n){
+        if(n%2){
+            res=res*x%mod;
+        }
+        n/=2;
+        x=x*x%mod;
+    }
+    return res;
+}
+
+//mod must be prime; O(log(mod))
+ll inverse_mod(ll x,ll mod){
+    x%=MOD;
+    return pow(x,mod-2,mod);
+}
+
+
+// N is the upper-bound of the numbers;
+const ll N=1e6+10,MOD=1e9+7;
+ll factorial[N];
+
+// precalculate factorial for all numbers < N; O(N)
+void factorial_precalculate(){
+    for(ll i=1;i<N;i++){
+        factorial[i]=factorial[i-1]*i%MOD;
+    }
+}
+
+//calculate n choose r for n,r < N; O(log(MOD))
+ll nCr(ll n,ll r){
+    return factorial[n]*inverse_mod(factorial[n-r]*factorial[r]%MOD,MOD);
+}
